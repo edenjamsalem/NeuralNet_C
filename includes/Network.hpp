cd9a33be072266d11fe8f09b91aa3637d438e290
edenbeden98 <edenbeden98@gmail.com>
2025-11-07 22:18:17 +0000

    minor updates

--- includes/Network.hpp
+++ includes/Network.hpp
@@ -21,10 +21,16 @@ struct LayerView {
 
 class NeuralNetwork {
 	private:
+		// Attributes
 		size_t num_layers;
 		std::vector<size_t> layer_sizes;
 		std::unique_ptr<float[]> buffer;
 		std::vector<LayerView> network;
+
+		// Methods
+		void feedForward(std::vector<float> image);
+		void adjust_parameters(size_t currentBatchCost);
+		void backProp();
 
 	public: 
 		// Constructors
@@ -33,7 +39,5 @@ class NeuralNetwork {
 
 		// Methods
 		void SGD(mnist::MNIST_dataset<std::__1::vector, std::__1::vector<float, std::__1::allocator<float>>, uint8_t> dataset);
-		void feedForward(std::vector<float> image);
-		void adjust_parameters(size_t currentBatchCost);
-		void backProp();
+
 };
